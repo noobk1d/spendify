7c8ebac7462854dc54367c193a14790843f8f6db
noobk1d <practicewithmi1@gmail.com>
2025-03-27 18:39:57 +0530

    Search Bar change

--- frontend/spendify/src/components/Testimonials.jsx
+++ frontend/spendify/src/components/Testimonials.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { Card } from "../components/ui/Dashboard/Card";
+import { Card } from "../components/ui/Dashboard/card";
 
 const testimonials = [
   {
